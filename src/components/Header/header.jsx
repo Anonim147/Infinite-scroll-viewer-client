e7cee02805ad7ee1248229f087b4def2657771a8
Alex Mykhailiuk <AleXXX14199@gmail.com>
2020-10-03 19:40:12 +0300

    remade footer & header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import logo from './logo.svg';
+
 import './header.scss';
 
 const Header = () => (
     <div className="Header">
-        <div className="logo"><h2>Infinite-scroll image viewer</h2></div>
+        <img src = {logo} />
     </div>
 );
 
